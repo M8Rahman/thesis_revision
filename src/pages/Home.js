41a37dacdbf53610c2586b3772fb6da07eb64cf2
kazi-ziaur-rahman-majba <majba9399@gmail.com>
2025-09-02 22:46:16 +0600

    fixed structure related issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css'
+import './App/App.css';
 // import { SiBlockchaindotcom } from "react-icons/si";
 
-import block from './images/blockchain.png'
+import block from '../images/blockchain.png';
 // import blockTo from '../public/images/blockchain.png'
 
 const Home = () => {
